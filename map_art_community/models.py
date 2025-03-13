3aafd5e9567a2bfdeb4e0b27975366429d55f18b
Yuxin Deng <yuxinden@andrew.cmu.edu>
2025-03-13 16:29:07 -0400

    add user models from previous homework

--- map_art_community/models.py
+++ map_art_community/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.conf import settings
 
-# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    profile_picture = models.FileField(blank=True, null=True)
+    following = models.ManyToManyField(User, related_name='followers')
+    bio = models.TextField(blank=True)
+    location = models.CharField(max_length=30, blank=True)
+    fname = models.CharField(max_length=30, blank=True)
+    lname = models.CharField(max_length=30, blank=True)
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
+
+class Follow(models.Model):
+    follower = models.ForeignKey(User, on_delete=models.CASCADE, related_name='following_set')
+    followed = models.ForeignKey(User, on_delete=models.CASCADE, related_name='follower_set')
+
+    def __str__(self):
+        return f'{self.follower.username} follows {self.followed.username}'
