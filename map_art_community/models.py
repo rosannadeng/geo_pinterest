e9cd91d8d5b35bb4e2454ee5e0fe5be9f9ee7794
Yuxin Deng <yuxinden@andrew.cmu.edu>
2025-03-17 23:14:36 -0400

    add basic html for profile, artwork, authentication; modify the artwork model only with locationname not latitude; add tests to every form; modify profile form so user no need to fill the fname lname agian

--- map_art_community/models.py
+++ map_art_community/models.py
@@ -1,22 +1,25 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.conf import settings
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.utils import timezone
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    profile_picture = models.FileField(upload_to='avatars/', blank=True, null=True)
-    following = models.ManyToManyField(User, related_name='followers')
-    bio = models.TextField(blank=True)
-    location = models.CharField(max_length=30, blank=True)
-    fname = models.CharField(max_length=30, blank=True)
-    lname = models.CharField(max_length=30, blank=True)
+    profile_picture = models.ImageField(upload_to='profile_pictures/', null=True, blank=True)
+    bio = models.TextField(max_length=500, blank=True)
+    location = models.CharField(max_length=100, blank=True)
     created_at = models.DateTimeField(default=timezone.now)
+    featured_artwork = models.ForeignKey(
+        'Artwork',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='featured_in_profiles'
+    )
 
     def __str__(self):
-        return f'{self.user.username}\'s Profile'
+        return f"{self.user.username}'s profile"
 
 class Artwork(models.Model):
     MEDIUM_CHOICES = [
@@ -35,19 +38,9 @@ class Artwork(models.Model):
     medium = models.CharField(max_length=3, choices=MEDIUM_CHOICES, default='OTH')
     creation_date = models.DateField()
     upload_date = models.DateTimeField(auto_now_add=True)
-    
-    # Location
-    latitude = models.FloatField()
-    longitude = models.FloatField()
     location_name = models.CharField(max_length=200)
-    street_view_url = models.URLField(blank=True)
-    
-    # Social 
     likes = models.ManyToManyField(User, related_name='liked_artworks', blank=True)
     views = models.PositiveIntegerField(default=0)
-    
-    class Meta:
-        ordering = ['-upload_date']
 
     def __str__(self):
         return self.title
@@ -55,19 +48,6 @@ class Artwork(models.Model):
     def total_likes(self):
         return self.likes.count()
 
-class Comment(models.Model):
-    artwork = models.ForeignKey(Artwork, on_delete=models.CASCADE, related_name='comments')
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
-    content = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
-
-    class Meta:
-        ordering = ['created_at']
-
-    def __str__(self):
-        return f'Comment by {self.author.username} on {self.artwork.title}'
-
-# Signal to create user profile
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
