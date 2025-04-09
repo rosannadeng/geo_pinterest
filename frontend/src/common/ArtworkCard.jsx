1da89e102e2a32e30cba7f228f62b3b9a274a7d8
yantongx <yantongx@andrew.cmu.edu>
2025-04-09 18:05:35 -0400

    Add actions of artworkcard in frontend

--- frontend/src/common/ArtworkCard.jsx
+++ frontend/src/common/ArtworkCard.jsx
@@ -1,31 +1,51 @@
-import React from 'react';
-import { Card, Avatar, Typography } from 'antd';
-import { UserOutlined, EnvironmentOutlined, CalendarOutlined } from '@ant-design/icons';
+import React, { useState } from 'react';
+import { Card, Avatar, Typography, Tooltip, Image } from 'antd';
+import { UserOutlined, EnvironmentOutlined, HeartOutlined, HeartFilled } from '@ant-design/icons';
+import { useNavigate } from 'react-router-dom';
+import './ArtworkCard.css';
 
 const { Meta } = Card;
 const { Text } = Typography;
 
-const ArtworkCard = ({ artwork }) => {
+const ArtworkCard = ({ artwork, onLike, likeCount }) => {
+  const [liked, setLiked] = useState(false);
+  const [likesCount, setLikesCount] = useState(likeCount || 0);
+
+  // todo: link to artist profile, handle my profile
+  const navigate = useNavigate();
+
+  const handleLike = () => {
+    if (!liked) {
+      setLikesCount(likesCount + 1);
+      setLiked(true);
+      if (onLike) {
+        onLike(artwork.id);
+      }
+    }
+  };
+
   return (
     <Card
       hoverable
-      cover={<img alt={artwork.title} src={artwork.image} />}
+      // cover={<img alt={artwork.title} src={artwork.image} />}
+      cover={<Image alt={artwork.title} src={artwork.image} />}
       actions={[
-        <UserOutlined key="artist" />,
-        <EnvironmentOutlined key="location" />,
-        <CalendarOutlined key="date" />
+        <UserOutlined key="artist" 
+        onClick={() => navigate(`/profile/${artwork.artist_id}`)}/>,
+         // todo: link to artist profile
+        <EnvironmentOutlined key="location" />, // todo: link to map view
+        <Tooltip title="Like">
+          { liked ? 
+          <HeartFilled key="like" style={{ color: 'red' }} onClick={handleLike} /> : 
+          <HeartOutlined key="like" onClick={handleLike} />}
+          <span>{likesCount}</span>
+        </Tooltip>
       ]}
     >
       <Meta
         title={artwork.title}
         description={artwork.description}
-        avatar={<Avatar src={artwork.artist.avatar} />}
       />
-      <div style={{ marginTop: 16 }}>
-        <Text type="secondary">Location: {artwork.location_name}</Text>
-        <br />
-        <Text type="secondary">Created: {artwork.creation_date}</Text>
-      </div>
     </Card>
   );
 };
