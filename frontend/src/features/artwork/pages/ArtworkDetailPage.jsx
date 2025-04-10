8fa0c19a0537979689422afcb915a9508cc4295d
yantongx <yantongx@andrew.cmu.edu>
2025-04-10 19:12:17 -0400

    Add homepage sider and map

--- frontend/src/features/artwork/pages/ArtworkDetailPage.jsx
+++ frontend/src/features/artwork/pages/ArtworkDetailPage.jsx
@@ -1,17 +1,51 @@
 // #Todo:add detail artwork page link user profile, artwork like
 import React, { useState, useEffect } from 'react';
-import { Layout } from 'antd';
-import ArtworkCard from '../../../common/ArtworkCard';
-// TODO: replace with getting artworks from backend
+import { Layout, Image, Card } from 'antd';
+import { useParams } from 'react-router-dom';
+import api from '../../../services/api';
+import { useAuth } from '../../../contexts/AuthContext';
+import { useNavigate } from 'react-router-dom';
 
-const artwork = { id: 1, title: 'Artwork 1', image: 'https://picsum.photos/200/300' }
 
 
-const ArtworkDetailPage = () => {
+const ArtworkDetailPage = ({ artworkId }) => {
+    const [liked, setLiked] = useState(false);
+    const [likesCount, setLikesCount] = useState(0);
+  
+    const { user } = useAuth();
+    const navigate = useNavigate();
+    const { id } = useParams();
+    console.log(id);
+    console.log(artworkId);
+    const [artwork, setArtwork] = useState(null);   
+
+    useEffect(() => {
+        const fetchArtwork = async () => {
+            try {
+                const response = await api.get(`/artwork/${id}`);
+                
+                setArtwork(response.data);
+                setLikesCount(response.data.total_likes || 0);
+                console.log('Fetching artwork with id:', id);
+                console.log('API response:', response.data);
+            } catch (error) {
+                console.error('Error fetching artwork:', error);
+            }
+        };
+        if (id) {
+            fetchArtwork();
+            console.log(artwork);
+        }
+    }, [id]);
+
+  
+
+
+
     return (
-        <Layout>
-                <ArtworkCard artwork={artwork} />
-        </Layout>
+        <Card>
+            {artwork && <Image src={artwork.image} />}
+        </Card>
     );
 };
 
