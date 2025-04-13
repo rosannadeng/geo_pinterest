dd3b70a33180387925d5d57e383d014b33ea0725
Karry Mao <karrym@andrew.cmu.edu>
2025-04-12 21:49:30 -0400

    fmt

--- frontend/src/common/AppSider.jsx
+++ frontend/src/common/AppSider.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Layout, Card , Flex} from 'antd';
+import { Layout, Card, Flex } from 'antd';
 import MasonryGrid from '../features/gallery/components/MasonryGrid';
 import ArtworkCard from './ArtworkCard';
 import api from '../services/api';
@@ -18,15 +18,16 @@ const AppSider = () => {
     };
     fetchArtworks();
   }, []);
-  
+
   return (
-    <Sider 
-      width={420} 
-      style={{ 
+    <Sider
+      width={420}
+      style={{
         backgroundColor: '#fff',
         padding: '24px',
         height: '95vh',   //could be code responsively
-        overflowY: 'auto' }}>
+        overflowY: 'auto'
+      }}>
       <Flex vertical gap={16}>
         {artworks.map((artwork) => (
           <ArtworkCard key={artwork.id} artwork={artwork} />
