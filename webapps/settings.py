9cbdf639b866a5651b18a75508b17b247c75ad23
Yuxin Deng <yuxinden@andrew.cmu.edu>
2025-04-15 21:32:08 -0400

    changed setting.py to deploy on aws

--- webapps/settings.py
+++ webapps/settings.py
@@ -225,11 +225,12 @@ CSRF_TRUSTED_ORIGINS = [
 ]
 
 # CORS settings
-CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [
-    'http://localhost:3000',
-    'http://127.0.0.1:3000',
+    'http://s25.cmu-webapps.com',
+    'http://localhost:3000',  
 ]
+
+CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOW_METHODS = [
     "DELETE",
     "GET",
