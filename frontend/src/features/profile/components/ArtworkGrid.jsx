49daa0151cbb3d53f568252628cc7409730fb6e4
Yuxin Deng <yuxinden@andrew.cmu.edu>
2025-04-16 22:28:15 -0400

    change the data model for featured artwork

--- frontend/src/features/profile/components/ArtworkGrid.jsx
+++ frontend/src/features/profile/components/ArtworkGrid.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Row, Col, Card, Image } from "antd";
+import { Row, Col } from "antd";
 import ArtworkCard from "../../../common/ArtworkCard";
 
 
@@ -9,21 +9,6 @@ const ArtworkGrid = ({ artworks }) => {
       {artworks.map((artwork) => (
         <Col xs={24} sm={12} md={8} lg={6} key={artwork.id}>
           <ArtworkCard artwork={artwork} style={{height:'200px'}}/>
-          {/* <Card
-            hoverable
-            cover={
-              <Image
-                alt={artwork.title}
-                src={artwork.image}
-                style={{ height: '200px', objectFit: 'cover' }}
-              />
-            }
-          >
-            <Card.Meta
-              title={artwork.title}
-              description={artwork.description}
-            />
-          </Card> */}
         </Col>
       ))}
     </Row>
